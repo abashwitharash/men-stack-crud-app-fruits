8ea117452a1e8e1863b505617c165fc6da58e34e
Arash <arashsaleh798@gmail.com>
2025-02-14 10:25:40 -0800

    added new.ejs and layout for new fruit

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const express = require('express');
 const mongoose = require('mongoose'); //
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI) // here we are able to use the functionality of mongoose 
+mongoose.connect(process.env.MONGODB_URI); // here we are able to use the functionality of mongoose 
 
 mongoose.connection.on('connected', () => {
     console.log(`connected on mondoDB ${mongoose.connection.name}`)
-})
+});
 //THIS GRABS THE CONNECTION BETWEEN FRUITS AND SERVER
 const Fruit = require('./models/fruit.js');
 
@@ -18,6 +18,16 @@ const Fruit = require('./models/fruit.js');
 app.get('/', async (req, res)=> {
     res.render('index.ejs');
 });
+
+
+//GET method /fruits/new 
+app.get('/fruits/new', (req, res) =>{
+    //this checks to see the route works with the res.send
+    // res.send('this route sends rhe user to a form page')
+    res.render('fruits/new.ejs') //we made a new folder called new.ejs and then grab it to make another page
+});
+
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
