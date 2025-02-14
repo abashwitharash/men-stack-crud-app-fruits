d5d95f3b1708bcaeac247fa4762aa727dcc10082
Arash <arashsaleh798@gmail.com>
2025-02-14 12:17:51 -0800

    add create functionality

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 // We begin by loading Express
 const dotenv = require('dotenv');
 dotenv.config();
+
 const express = require('express');
 const mongoose = require('mongoose'); //
+
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI); // here we are able to use the functionality of mongoose 
@@ -13,6 +15,9 @@ mongoose.connection.on('connected', () => {
 });
 //THIS GRABS THE CONNECTION BETWEEN FRUITS AND SERVER
 const Fruit = require('./models/fruit.js');
+
+//middleware express is the library available - 
+app.use(express.urlencoded({ extended: false}));
 
 //Get  // why do we async?
 app.get('/', async (req, res)=> {
@@ -27,7 +32,18 @@ app.get('/fruits/new', (req, res) =>{
     res.render('fruits/new.ejs') //we made a new folder called new.ejs and then grab it to make another page
 });
 
+//POST /fruits
+//async because its a database transaction  - whateve that means 
+app.post('/fruits', async (req, res) => {
+    if (req.body.isReadyToEat === 'on') {
+        req.body.isReadyToEat = true; 
+    } else {
+        req.body.isReadyToEat = false;
+    }
 
+    await Fruit.create(req.body);
+    res.redirect('/fruits/new') //this redirect the fruits-new to the fruits 
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
